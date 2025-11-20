09d8a7b178a902b554b9dc4a156bbf8d093989cc
sfarrukhm <en1.farrukh@gmail.com>
2025-11-20 16:54:22 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import mlflow
+from mlflow.models import infer_signature
+
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+X, y = datasets.load_iris(return_X_y=True)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Define the model hyperparameters
+params={
+    "solver":"lbfgs",
+    "max_iter":1000,
+    "random_state":8888
+}
+
+# train the model
+model = LogisticRegression(**params)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model accuracy: {accuracy}")
+
+# save trained model locally
+mlflow.sklearn.save_model(model, "logistic_regression_model")
